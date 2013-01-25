56f35ef77ba3ac8dfcadfde33964f62a17e14b2c
James Henderson <james@pcr.me.uk>
2013-01-25 08:36:53 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# xmlt
+
+A Clojure XML library that needs a lot better description
+
+## Usage
+
+TODO
+
+## License
+
+Copyright © 2013 James Henderson
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jarohen/xmlt "0.1.0"
+  :description "An XML transformation library for Clojure"
+  :url "https://github.com/james-henderson/xmlt"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
