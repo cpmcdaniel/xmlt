f21f5654823b1a286d10d26761657c5f8ebe55bb
James Henderson <james@pcr.me.uk>
2013-01-25 10:57:16 +0000

    Written a 'split-to-matching-element' fn that lazily splits a stream of xml events into 'this tag' and 'everything after'

--- src/xmlt/core.clj
+++ src/xmlt/core.clj
@@ -2,7 +2,7 @@
   (:import [javax.xml.stream
             XMLInputFactory XMLOutputFactory]
            [javax.xml.stream.events
-            StartElement]))
+            StartElement EndElement]))
 
 (def input-factory ^XMLInputFactory (XMLInputFactory/newFactory))
 (def output-factory ^XMLOutputFactory (XMLOutputFactory/newFactory))
@@ -32,9 +32,32 @@
 (defn traverse [events & {:keys [ctx handlers]}]
   )
 
-(traverse (xml->events (java.io.StringReader. "<foo><bar>Hello world</bar></foo>"))
-          :ctx {:key :v
-                :evs []}
-          :handlers [(fn [events {:keys [ctx] :as handler-info}]
-                       (let [[e & more] events]
-                         [e (update-in ctx [:evs] conj {:ev e}) more]))])
+(defn split-to-matching-element [events]
+  (letfn [(split-to-matching-element* [depth before-matching-event after-matching-event]
+          (lazy-seq
+           (if (seq after-matching-event)
+             (let [e (first after-matching-event)
+                   more (lazy-seq (rest after-matching-event))]
+               (condp instance? e
+                 StartElement (split-to-matching-element* (inc depth) (lazy-cat before-matching-event [e]) more)
+                 EndElement (if (zero? depth)
+                              [before-matching-event more]
+                              (split-to-matching-element* (dec depth) (lazy-cat before-matching-event [e]) more))
+                 (split-to-matching-element* depth (lazy-cat before-matching-event [e]) more)))
+             [before-matching-event nil])))]
+    (split-to-matching-element* 0 nil events)))
+
+#_(let [[before after] (split-to-matching-element
+                        (drop 2 (xml->events
+                                 (java.io.StringReader.
+                                  "<foo><bar>Hello world</bar><bar>Hello world</bar><bar>Hello world</bar><bar>Hello world</bar></foo><blowp><Wraqw>"))))]
+  before)
+
+
+
+#_(traverse (xml->events (java.io.StringReader. "<foo><bar>Hello world</bar></foo>"))
+            :ctx {:key :v
+                  :evs []}
+            :handlers [(fn [events {:keys [ctx] :as handler-info}]
+                         (let [[e & more] events]
+                           [e (update-in ctx [:evs] conj {:ev e}) more]))])
