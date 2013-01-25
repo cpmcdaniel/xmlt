6c2a7dede1c4d8b21fd6b96676c22f32aad4ab2e
James Henderson <james@pcr.me.uk>
2013-01-25 08:50:26 +0000

    Simple xml->events and events->xml

--- src/xmlt/core.clj
+++ src/xmlt/core.clj
@@ -1,6 +1,21 @@
-(ns xmlt.core)
+(ns xmlt.core
+  (:import [javax.xml.stream
+            XMLInputFactory XMLOutputFactory]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def input-factory ^XMLInputFactory (XMLInputFactory/newFactory))
+(def output-factory ^XMLOutputFactory (XMLOutputFactory/newFactory))
+
+(defn xml->events [reader-or-stream]
+  ;; TODO encoding?
+  (let [event-reader (. input-factory (createXMLEventReader reader-or-stream))]
+    (iterator-seq event-reader)))
+
+(defn events->xml [events writer-or-stream]
+  ;; TODO again, encoding...
+  (with-open [event-writer (. output-factory (createXMLEventWriter writer-or-stream))]
+    (doseq [event events]
+      (. event-writer (add event)))))
+
+#_(let [w (java.io.StringWriter.)]
+    (events->xml (xml->events (java.io.StringReader. "<foo><bar>Hello world</bar></foo>")) w)
+    (str w))
