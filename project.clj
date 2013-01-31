43010ca00d7b372ed0e8fb018e49533f749bd030
James Henderson <james@pcr.me.uk>
2013-01-31 21:10:50 +0000

    Added Lamina into the deps

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/james-henderson/xmlt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [lamina "0.5.0-beta9"]])
