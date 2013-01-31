ab2e825073aaa429451effe7254b3a220dfe2255
James Henderson <james@pcr.me.uk>
2013-01-31 22:53:41 +0000

    Removed the dependence on Lamina - wasn't entirely necessary and think it was causing a memory leak...

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "https://github.com/james-henderson/xmlt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lamina "0.5.0-beta9"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
--- src/xmlt/core.clj
+++ src/xmlt/core.clj
@@ -1,9 +1,6 @@
 (ns xmlt.core
-  (:require [lamina
-             [core :as l]
-             [executor :as lex]])
   (:import [javax.xml.stream
-            XMLInputFactory XMLOutputFactory XMLEventFactory]
+            XMLInputFactory XMLOutputFactory XMLEventFactory XMLEventReader XMLEventWriter]
            [javax.xml.stream.events
             StartElement EndElement Characters]))
 
@@ -12,121 +9,100 @@
 (def output-factory ^XMLOutputFactory (XMLOutputFactory/newFactory))
 (def event-factory ^XMLEventFactory (XMLEventFactory/newFactory))
 
-(defn xml->events [reader-or-stream]
+(defn open-xml-reader [reader-or-stream]
   ;; TODO encoding?
-  (let [event-reader (. input-factory (createXMLEventReader reader-or-stream))]
-    {:reader event-reader
-     :events (iterator-seq event-reader)}))
+  (. input-factory (createXMLEventReader reader-or-stream)))
 
-(defn events->xml [events writer-or-stream]
+(defn open-xml-writer [writer-or-stream]
   ;; TODO again, encoding...
-  (with-open [event-writer (. output-factory (createXMLEventWriter writer-or-stream))]
-    (doseq [event events]
-      (. event-writer (add event)))))
+  (. output-factory (createXMLEventWriter writer-or-stream)))
 
-(defn transform-tag-content [ch current-tag & {:keys [ctx path-transformers]}]
-  (l/enqueue ch current-tag)
-  (l/run-pipeline {:ctx ctx :path []}
-                  (fn [{:keys [ctx path] :as m}]
-                    (l/run-pipeline (l/read-channel ch)
-                                    (fn [ev] (assoc m :ev ev))))
+(def ^:dynamic *r* nil)
+(def ^:dynamic *w* nil)
 
-                  (fn [{:keys [ctx path ev] :as m}]
-                    (condp instance? ev
-                      StartElement (let [start-el-name (keyword (.. ev getName getLocalPart))
-                                         new-path (conj path {:tag start-el-name})]
-                                     (if-let [transformer (get path-transformers (map :tag new-path))]
-                                       (let [after-ctx @(l/run-pipeline (transformer :ch ch :current-tag ev :ctx ctx))]
-                                         (l/restart {:ctx after-ctx :path path}))
-                                       (do (l/enqueue ch ev)
-                                           (l/restart {:ctx ctx :path new-path}))))
+(defn transform-tag-content [& {:keys [ctx path-transformers]}]
+  (. *w* (add (.nextEvent *r*)))
+  (loop [{:keys [ctx path] :as m} {:ctx ctx :path []}]
+    (let [ev (.peek *r*)]
+      (condp instance? ev
+        StartElement (let [start-el-name (keyword (.. ev getName getLocalPart))
+                           new-path (conj path {:tag start-el-name})]
+                       (if-let [transformer (get path-transformers (map :tag new-path))]
+                         (recur {:ctx (transformer :ctx ctx) :path path})
+                         (do (.add *w* (.nextEvent *r*))
+                             (recur {:ctx ctx :path new-path}))))
 
-                      Characters (let [text (.. ev getData)
-                                       new-path (conj path {:tag 'text})]
-                                   (if-let [transformer (get path-transformers (map :tag new-path))]
-                                     (let [after-ctx @(l/run-pipeline (transformer :ch ch :text text :ctx ctx))]
-                                       (l/restart {:ctx after-ctx :path path}))
-                                     (do (l/enqueue ch ev)
-                                         (l/restart m))))
+        Characters (let [ev (.nextEvent *r*)
+                         text (.. ev getData)
+                         new-path (conj path {:tag 'text})]
+                     (if-let [transformer (get path-transformers (map :tag new-path))]
+                       (recur {:ctx (transformer :text text :ctx ctx) :path path})
+                       (do (.add *w* ev)
+                           (recur m))))
 
-                      EndElement (if (seq path)
-                                   (do (l/enqueue ch ev)
-                                       (l/restart {:ctx ctx :path (vec (butlast path))}))
+        EndElement (let [ev (.nextEvent *r*)]
+                     (if (seq path)
+                       (do (.add *w* ev)
+                           (recur {:ctx ctx :path (vec (butlast path))}))
 
-                                   (let [after-ctx (if-let [after-transformer (get path-transformers :after)]
-                                                     (after-transformer :ctx ctx :ch ch)
-                                                     ctx)]
-                                     (l/enqueue ch ev)
-                                     (l/complete after-ctx)))
+                       (let [after-ctx (if-let [after-transformer (get path-transformers :after)]
+                                         (after-transformer :ctx ctx)
+                                         ctx)]
+                         (.add *w* ev)
+                         after-ctx)))
 
-                      (do (l/enqueue ch ev)
-                          (l/restart m))))))
+        (do (.add *w* (.nextEvent *r*))
+            (recur m))))))
 
-(defn add-str [ch s]
-  (l/enqueue ch (. event-factory (createCharacters s))))
+(defn add-str [s]
+  (.add *w* (. event-factory (createCharacters s))))
 
-(defn add-tag [ch element]
+(defn add-tag [element]
   ;; TODO doesn't handle attributes yet.
   (cond
    (vector? element) (let [[tag & content] element]
-                       (l/enqueue ch (. event-factory (createStartElement "" "" (name tag))))
-                       (add-tag ch content)
-                       (l/enqueue ch (. event-factory (createEndElement "" "" (name tag)))))
-   (string? element) (l/enqueue ch (. event-factory (createCharacters element)))
-   (seq? element) (doseq [e element] (add-tag ch e))
-   :otherwise (add-tag ch (str element))))
+                       (.add *w* (. event-factory (createStartElement "" "" (name tag))))
+                       (add-tag content)
+                       (.add *w* (. event-factory (createEndElement "" "" (name tag)))))
+   (string? element) (.add *w* (. event-factory (createCharacters element)))
+   (seq? element) (doseq [e element] (add-tag e))
+   :otherwise (add-tag (str element))))
 
 (defn transform-file [in-stream out-writer transformer]
-  (let [[our-ch their-ch] (l/channel-pair)
-        {:keys [events reader]} (xml->events in-stream)
-        [start-doc start-root-tag & more] events
-        renderer-task (lex/task
-                       (events->xml (l/channel->lazy-seq our-ch)
-                                    out-writer))]
+  (with-open [r (open-xml-reader in-stream)
+              w (open-xml-writer out-writer)]
+    (binding [*r* r
+              *w* w]
+      (let [start-doc (.nextEvent r)]
 
-    (l/siphon (l/lazy-seq->channel more) our-ch)
+        (.add w start-doc)
 
-    (l/enqueue their-ch start-doc)
-            
-    (l/run-pipeline nil
-                    {:error-handler (fn [_] (.close reader))}
-                    (fn [_]
-                      (transformer their-ch start-root-tag))
+        (let [ctx (transformer)]
 
-                    (fn [ctx]
-                      (l/run-pipeline nil
-                                      (fn [_] (l/read-channel their-ch))
+          (.add w (.nextEvent r)) ;; end element
 
-                                      (fn [end-doc]
-                                        (l/enqueue their-ch end-doc)
-                                        (l/close their-ch)
-                                        (l/on-drained their-ch #(.close reader)))
-                                      
-                                      ;; await the renderer task
-                                      (constantly renderer-task)
+          ;; return the context
+          ctx)))))
 
-                                      ;; return the context
-                                      (constantly ctx))))))
+#_(let [sw (java.io.StringWriter.)
+      sr (java.io.StringReader. "<root><hello><world>Hello world</world><world>Hello world again</world></hello></root>")]
+  (transform-file sr sw
+                  (fn []
+                    (transform-tag-content
+                     :path-transformers
+                     {[:hello]
+                      (fn [& _]
+                        (transform-tag-content
+                         :path-transformers {[:world]
+                                             (fn [& {:keys [ctx]}]
+                                               (transform-tag-content
+                                                :ctx ctx
+                                                :path-transformers {['text]
+                                                                    (fn [& {:keys [text ctx]}]
+                                                                      (add-str (apply str (reverse text)))
+                                                                      (update-in ctx [:worlds] conj 1))}))
 
-(let [sw (java.io.StringWriter.)
-      sr (java.io.StringReader. "<root><hello><world>Text</world><world>More text</world><world>Even more text</world></hello></root>")]
-  @(transform-file sr sw
-                   (fn [ch current-tag]
-                     (transform-tag-content ch current-tag
-                                            :path-transformers
-                                            {[:hello]
-                                             (fn [& {:keys [ch current-tag ctx]}]
-                                               (transform-tag-content ch current-tag
-                                                                      :path-transformers {[:world]
-                                                                                          (fn [& {:keys [ch current-tag ctx]}]
-                                                                                            (transform-tag-content ch current-tag
-                                                                                                                   :ctx ctx
-                                                                                                                   :path-transformers {['text]
-                                                                                                                                       (fn [& {:keys [ch text ctx]}]
-                                                                                                                                         (add-str ch (apply str (reverse text)))
-                                                                                                                                         (update-in ctx [:worlds] conj 1))}))
-
-                                                                                          :after
-                                                                                          (fn [& {:keys [ctx]}]
-                                                                                            (add-tag ch [:world-count (count (:worlds ctx))]))}))})))
+                                             :after
+                                             (fn [& {:keys [ctx]}]
+                                               (add-tag [:world-count (count (:worlds ctx))]))}))})))
   (str sw))
